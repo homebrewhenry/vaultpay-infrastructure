423e5eeece22c09744086c9e1e5d71df417b55bc
Homebrew Henry <209376467+homebrewhenry@users.noreply.github.com>
2026-03-19 15:36:11 +0000

    Add Vaultpay VPC

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/20"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name    = "vaultpay-vpc"
+    Project = "VaultPay"
+  }
+}
