e9ba5e2b3a6b61f34ee49060158de69de23e6391
Homebrew Henry <209376467+homebrewhenry@users.noreply.github.com>
2026-03-24 19:31:02 +0000

    Add public subnets with variables

--- main.tf
+++ main.tf
@@ -3,12 +3,34 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/20"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
     Name    = "vaultpay-vpc"
+    Project = "VaultPay"
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[0]
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name    = "vaultpay-public-subnet-a"
+    Project = "VaultPay"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnet_cidrs[1]
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name    = "vaultpay-public-subnet-b"
     Project = "VaultPay"
   }
 }
